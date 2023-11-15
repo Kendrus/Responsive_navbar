0172e63f86f469a583d04b48a483b67997ae303a
kendra <sidoinelagrande@gmail.com>
2023-11-15 13:33:27 +0000

    responsive nav

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+let hamburger = document.querySelector(".hamburger");
+console.log("dfdfdfd");
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
